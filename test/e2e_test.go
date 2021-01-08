7e686a9ce61102b6eb5bcc6c76693bd5c51b360a
João Aldeano <jotaldeano@gmail.com>
2021-01-08 14:36:21 +0000

    added e2e client/server test

--- test/e2e_test.go
+++ test/e2e_test.go
@@ -21,7 +21,7 @@ func TestGetID(t *testing.T) {
 
 	go clientX.assertGetIDResponseFromServer(t)
 	go clientX.WS.WriteMessage(1, []byte("id"))
-	time.Sleep(time.Second * 2) // give some breathing room to receive the server response
+	time.Sleep(time.Second * 1) // give some breathing room to receive the server response
 	clientX = nil
 }
 
@@ -35,10 +35,21 @@ func TestGetList(t *testing.T) {
 
 	go clientX.assertGetListResponseFromServer(t)
 	go clientX.WS.WriteMessage(1, []byte("list"))
-	time.Sleep(time.Second * 2) // give some breathing room to receive the server response
+	time.Sleep(time.Second * 1) // give some breathing room to receive the server response
 }
 
 func TestRelay(t *testing.T) {
+	testChannel := make(chan string)
+	address := "localhost:8082"
+	go msgSystemHub.InitHub(address)
+	clientX := newTestClient(address)
+	_ = newTestClient(address) // create another client, otherwise only the client X will be connected and list will be empty
+
+	go clientX.assertRelayMessage(t, testChannel)
+	go clientX.getUserIDsAndAttachRelay(testChannel)
+	clientX.WS.WriteMessage(1, []byte("list"))
+	time.Sleep(time.Second * 1) // give some breathing room to receive the server response
+
 }
 
 type TestClient struct {
@@ -61,6 +72,49 @@ func newTestClient(address string) *TestClient {
 
 }
 
+func (c *TestClient) getUserIDsAndAttachRelay(listUserIDChannel chan string) {
+	for {
+		select {
+		case msg := <-listUserIDChannel:
+			relayMsg := fmt.Sprintf("relay|users=%s,body=hello world", msg)
+			c.WS.WriteMessage(1, []byte(relayMsg))
+		}
+	}
+}
+
+func (c *TestClient) assertRelayMessage(t *testing.T, listUserIDChannel chan string) {
+	for {
+		select {
+		case msg := <-c.Data:
+			if strings.HasPrefix(string(msg), "server: users list:") {
+				portString := strings.TrimPrefix(string(msg), "server: users list: \n0) ")
+				portString = strings.TrimSuffix(portString, "\n")
+				_, err := strconv.Atoi(portString)
+				if err != nil {
+					t.Fatalf("unexpected response from server: user id expected to be a number, got %s, err: %v", string(msg), err)
+				}
+				listUserIDChannel <- portString
+			} else {
+				// the server does not respond to  user when he sends relay messages
+				t.Fatal(msg)
+			}
+		}
+	}
+}
+
+func (c *TestClient) assertGetIDResponseFromServer(t *testing.T) {
+	for {
+		select {
+		case msg := <-c.Data:
+			if !strings.HasPrefix(string(msg), "server: ") {
+				t.Fatalf("unexpected response from server: expected to be prefixed by 'server: ', got %s", string(msg))
+			}
+
+		}
+
+	}
+}
+
 func (c *TestClient) assertGetListResponseFromServer(t *testing.T) {
 	for {
 		select {
@@ -75,18 +129,6 @@ func (c *TestClient) assertGetListResponseFromServer(t *testing.T) {
 			if err != nil {
 				t.Fatalf("unexpected response from server: user id expected to be a number, got %s, err: %v", string(msg), err)
 			}
-		}
-	}
-}
-
-func (c *TestClient) assertGetIDResponseFromServer(t *testing.T) {
-	for {
-		select {
-		case msg := <-c.Data:
-			if !strings.HasPrefix(string(msg), "server: ") {
-				t.Fatalf("unexpected response from server: expected to be prefixed by 'server: ', got %s", string(msg))
-			}
-
 		}
 	}
 }
